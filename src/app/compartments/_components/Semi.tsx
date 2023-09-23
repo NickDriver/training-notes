45d57dfc2a61b44a567e0b44ceeb5772cddc2ea5
Nick Chelabchi <mykolay.chelabchi@gmail.com>
2023-09-23 05:34:23 -0700

    data.ts added

--- src/app/compartments/_components/Semi.tsx
+++ src/app/compartments/_components/Semi.tsx
@@ -1,70 +1,23 @@
 import { Compartment, Compartment_t, Vendors, TruckType } from "./Compartment";
+import { data_shell, data_vernon } from "../_data/data";
 
 
-const comp_1: Compartment_t = {
-  compartment: 1,
-  size: 3500,
-  vendor: Vendors.SHELL,
-  truck_type: TruckType.SEMI,
-}
-const comp_2: Compartment_t = {
-  compartment: 2,
-  size: 1700,
-  vendor: Vendors.SHELL,
-  truck_type: TruckType.SEMI,
-}
-const comp_3: Compartment_t = {
-  compartment: 3,
-  size: 1000,
-  vendor: Vendors.SHELL,
-  truck_type: TruckType.SEMI,
-}
-const comp_4: Compartment_t = {
-  compartment: 4,
-  size: 2000,
-  vendor: Vendors.SHELL,
-  truck_type: TruckType.SEMI,
-}
-const ver_1: Compartment_t = {
-  compartment: 1,
-  size: 3500,
-  vendor: Vendors.VERNON,
-  truck_type: TruckType.SEMI,
-}
-const ver_2: Compartment_t = {
-  compartment: 2,
-  size: 1700,
-  vendor: Vendors.VERNON,
-  truck_type: TruckType.SEMI,
-}
-const ver_3: Compartment_t = {
-  compartment: 3,
-  size: 1000,
-  vendor: Vendors.VERNON,
-  truck_type: TruckType.SEMI,
-}
-const ver_4: Compartment_t = {
-  compartment: 4,
-  size: 2000,
-  vendor: Vendors.VERNON,
-  truck_type: TruckType.SEMI,
-}
 
 export default function Semi() {
   return (
     <div className="flex flex-col items-center">
       <div>SEMI</div>
       <div className="flex justify-center flex-row-reverse m-1 w-96">
-        <Compartment compartment_info={comp_1} />
-        <Compartment compartment_info={comp_2} />
-        <Compartment compartment_info={comp_3} />
-        <Compartment compartment_info={comp_4} />
+        <Compartment compartment_info={data_shell[0]} />
+        <Compartment compartment_info={data_shell[1]} />
+        <Compartment compartment_info={data_shell[2]} />
+        <Compartment compartment_info={data_shell[3]} />
       </div>
       <div className="flex justify-center flex-row-reverse m-1 w-96">
-        <Compartment compartment_info={ver_1} />
-        <Compartment compartment_info={ver_2} />
-        <Compartment compartment_info={ver_3} />
-        <Compartment compartment_info={ver_4} />
+        <Compartment compartment_info={data_vernon[0]} />
+        <Compartment compartment_info={data_vernon[1]} />
+        <Compartment compartment_info={data_vernon[2]} />
+        <Compartment compartment_info={data_vernon[3]} />
       </div>
     </div>
   )
---
+++ src/app/compartments/_data/data.ts
@@ -0,0 +1,55 @@
+import { Compartment_t, TruckType, Vendors } from "../_components/Compartment"
+
+export const data_shell: Compartment_t[] = [
+  {
+    compartment: 1,
+    size: 3500,
+    vendor: Vendors.SHELL,
+    truck_type: TruckType.SEMI,
+  },
+  {
+    compartment: 2,
+    size: 1700,
+    vendor: Vendors.SHELL,
+    truck_type: TruckType.SEMI,
+  },
+  {
+    compartment: 3,
+    size: 1000,
+    vendor: Vendors.SHELL,
+    truck_type: TruckType.SEMI,
+  },
+  {
+    compartment: 4,
+    size: 2000,
+    vendor: Vendors.SHELL,
+    truck_type: TruckType.SEMI,
+  }
+]
+
+export const data_vernon: Compartment_t[] = [
+  {
+    compartment: 1,
+    size: 3500,
+    vendor: Vendors.VERNON,
+    truck_type: TruckType.SEMI,
+  },
+  {
+    compartment: 2,
+    size: 1700,
+    vendor: Vendors.VERNON,
+    truck_type: TruckType.SEMI,
+  },
+  {
+    compartment: 3,
+    size: 1000,
+    vendor: Vendors.VERNON,
+    truck_type: TruckType.SEMI,
+  },
+  {
+    compartment: 4,
+    size: 2000,
+    vendor: Vendors.VERNON,
+    truck_type: TruckType.SEMI,
+  }
+]
