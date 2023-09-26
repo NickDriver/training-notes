a4d4142c2b3cbe281914361b1b2d36334611025b
Nick Chelabchi <mykolay.chelabchi@gmail.com>
2023-09-26 16:23:01 -0700

    Clean up

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -7,9 +7,6 @@ export default function NavBar() {
         <ul className="flex">
           <li className="p-2 underline"><Link href="/sizes">Sizes</Link></li>
           <li className="p-2 underline"><Link href="/compartments">Compartments</Link></li>
-          <li className="p-2 underline"><Link href="http://google.com">Google</Link></li>
-          <li className="p-2">SomeThing-2</li>
-          <li className="p-2">SomeThing-3</li>
         </ul>
         <ul className="flex">
           <li className="underline p-2 "><Link href="/">HOME</Link></li>
