7feab8316edce0fac41ccca0a540ec52a5216287
Nick Chelabchi <mykolay.chelabchi@gmail.com>
2023-09-28 01:45:21 -0700

    clean Up

--- src/app/Breakdown/_components/Practice/_components/Result.tsx
+++ src/app/Breakdown/_components/Practice/_components/Result.tsx
@@ -1,4 +1,5 @@
 export default function Result() {
+  console.log();
   return (
     <div className="flex justify-center items-center m-2 w-full h-full bg-slate-800 rounded-md ">
       Result - colors temporary
