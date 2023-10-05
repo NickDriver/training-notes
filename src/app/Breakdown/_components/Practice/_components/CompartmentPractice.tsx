b46c41ab09a39fe9cfc424edd473b08c30343cc4
Nick Chelabchi <mykolay.chelabchi@gmail.com>
2023-10-05 15:09:24 -0700

    hover removed

--- src/app/Breakdown/_components/Practice/_components/CompartmentPractice.tsx
+++ src/app/Breakdown/_components/Practice/_components/CompartmentPractice.tsx
@@ -4,7 +4,7 @@ import { ProductLabel, TruckType, CompartmentNumber, Product } from "./products/
 let comp: CompartmentNumber;
 export default function CompartmentPractice({ comp, gallons, truck_type }: { comp: CompartmentNumber, gallons: any, truck_type: TruckType }) {
   return (
-    <div className="flex flex-col items-center w-full m-2 p-1 bg-slate-500 text-slate-900 rounded-md hover:bg-red-300">
+    <div className="flex flex-col items-center w-full m-2 p-1 bg-slate-500 text-slate-900 rounded-md">
       <div className="font-bold">{comp.toString()}</div>
       <ProductLabel comp={comp} truck_type={truck_type} product={Product.GAS} />
       <ProductLabel comp={comp} truck_type={truck_type} product={Product.DSL} />
