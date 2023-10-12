d3e9a49fc807085b6aa5dae69f7f95b7c6e5f2c0
Nick Chelabchi <mykolay.chelabchi@gmail.com>
2023-10-12 11:23:41 -0700

    disable done

--- src/app/Breakdown/_components/Practice/_components/Choice.tsx
+++ src/app/Breakdown/_components/Practice/_components/Choice.tsx
@@ -1,4 +1,5 @@
 import { ProductObject, Products } from "@/app/Breakdown/_data/constants";
+import { useState } from "react";
 
 enum Color {
   REG = " bg-slate-400",
@@ -10,47 +11,115 @@ enum Color {
 }
 
 function ProductsProvided({ gallons, chosenProductHandler }: { gallons: ProductObject[], chosenProductHandler: any }) {
+  // States for disable input
+  const [isRegularVisible, setIsRegularVisible] = useState(false);
+  const [isMidgradeVisible, setIsMidgradeVisible] = useState(false);
+  const [isPremiumVisible, setIsPremiumVisible] = useState(false);
+  const [isE85Visible, setIsE85Visible] = useState(false);
+  const [isDieselVisible, setIsDieselVisible] = useState(false);
+  const [isEthanolVisible, setIsEthanolVisible] = useState(false);
 
   // Handler to lift state up: ProductsProvided => Choice
   function productHandler(data: ProductObject) {
     chosenProductHandler(data);
   }
 
+  // Handlers for change
+  const onRegular = (event: any) => {
+    productHandler({ name: Products.regular, voluem: Number(event.target.value) });
+    setIsRegularVisible(false);
+    setIsMidgradeVisible(true);
+    setIsPremiumVisible(true);
+    setIsE85Visible(true);
+    setIsDieselVisible(true);
+    setIsEthanolVisible(true);
+  }
+
+  const onMidgrade = (event: any) => {
+    productHandler({ name: Products.midgrade, voluem: Number(event.target.value) });
+    setIsRegularVisible(true);
+    setIsMidgradeVisible(false);
+    setIsPremiumVisible(true);
+    setIsE85Visible(true);
+    setIsDieselVisible(true);
+    setIsEthanolVisible(true);
+  }
+
+  const onPremium = (event: any) => {
+    productHandler({ name: Products.premium, voluem: Number(event.target.value) });
+    setIsRegularVisible(true);
+    setIsMidgradeVisible(true);
+    setIsPremiumVisible(false);
+    setIsE85Visible(true);
+    setIsDieselVisible(true);
+    setIsEthanolVisible(true);
+  }
+
+  const onE85 = (event: any) => {
+    productHandler({ name: Products.e85, voluem: Number(event.target.value) });
+    setIsRegularVisible(true);
+    setIsMidgradeVisible(true);
+    setIsPremiumVisible(true);
+    setIsE85Visible(false);
+    setIsDieselVisible(true);
+    setIsEthanolVisible(true);
+  }
+
+  const onDiesel = (event: any) => {
+    productHandler({ name: Products.diesel, voluem: Number(event.target.value) });
+    setIsRegularVisible(true);
+    setIsMidgradeVisible(true);
+    setIsPremiumVisible(true);
+    setIsE85Visible(true);
+    setIsDieselVisible(false);
+    setIsEthanolVisible(true);
+  }
+
+  const onEthanol = (event: any) => {
+    productHandler({ name: Products.ethanol, voluem: Number(event.target.value) });
+    setIsRegularVisible(true);
+    setIsMidgradeVisible(true);
+    setIsPremiumVisible(true);
+    setIsE85Visible(true);
+    setIsDieselVisible(true);
+    setIsEthanolVisible(false);
+  }
+
   const show = gallons.map((product) => {
     if (product.name === Products.regular) {
       return <div className={"flex w-full justify-center p-2 " + Color.REG} key={"01"}>
-        <input className="placeholder:text-center w-3/4 pl-1" placeholder="87" type="number"
-          onChange={(event: any) => { productHandler({ name: Products.regular, voluem: Number(event.target.value) }) }} />
+        <input disabled={isRegularVisible} className="placeholder:text-center w-3/4 pl-1" placeholder="87" type="number"
+          onChange={onRegular} />
       </div>;
     }
     if (product.name === Products.midgrade) {
       return <div className={"flex w-full justify-center p-2 " + Color.MID} key={"02"}>
-        <input className="placeholder:text-center w-3/4 pl-1" placeholder="89" type="number" min={0} max={4000}
-          onChange={(event: any) => { productHandler({ name: Products.midgrade, voluem: Number(event.target.value) }) }} />
+        <input disabled={isMidgradeVisible} className="placeholder:text-center w-3/4 pl-1" placeholder="89" type="number" min={0} max={4000}
+          onChange={onMidgrade} />
       </div>;
     }
     if (product.name === Products.premium) {
       return <div className={"flex w-full justify-center p-2 " + Color.PRM} key={"03"}>
-        <input className="placeholder:text-center w-3/4 pl-1" placeholder="91" type="number" min={0} max={4000}
-          onChange={(event: any) => { productHandler({ name: Products.premium, voluem: Number(event.target.value) }) }} />
+        <input disabled={isPremiumVisible} className="placeholder:text-center w-3/4 pl-1" placeholder="91" type="number" min={0} max={4000}
+          onChange={onPremium} />
       </div>;
     }
     if (product.name === Products.e85) {
       return <div className={"flex w-full justify-center p-2 " + Color.E85} key={"05"}>
-        <input className="placeholder:text-center w-3/4 pl-1" placeholder="E85" type="number" min={0} max={4000}
-          onChange={(event: any) => { productHandler({ name: Products.e85, voluem: Number(event.target.value) }) }} />
+        <input disabled={isE85Visible} className="placeholder:text-center w-3/4 pl-1" placeholder="E85" type="number" min={0} max={4000}
+          onChange={onE85} />
       </div>;
     }
     if (product.name === Products.diesel) {
       return <div className={"flex w-full justify-center p-2 " + Color.DSL} key={"04"}>
-        <input className="placeholder:text-center w-3/4 pl-1" placeholder="DIESEL" type="number" min={0} max={4000}
-          onChange={(event: any) => { productHandler({ name: Products.diesel, voluem: Number(event.target.value) }) }} />
+        <input disabled={isDieselVisible} className="placeholder:text-center w-3/4 pl-1" placeholder="DIESEL" type="number" min={0} max={4000}
+          onChange={onDiesel} />
       </div>;
     }
     if (product.name === Products.ethanol) {
       return <div className={"flex w-full justify-center p-2 " + Color.ETH} key={"06"}>
-        <input className="placeholder:text-center w-3/4 pl-1" placeholder="ETHANOL" type="number" min={0} max={4000}
-          onChange={(event: any) => { productHandler({ name: Products.ethanol, voluem: Number(event.target.value) }) }} />
+        <input disabled={isEthanolVisible} className="placeholder:text-center w-3/4 pl-1" placeholder="ETHANOL" type="number" min={0} max={4000}
+          onChange={onEthanol} />
       </div>;
     }
   });
