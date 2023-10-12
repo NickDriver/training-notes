925397a4c08cc0ed56a767385462cfab4633c1d6
Nick Chelabchi <mykolay.chelabchi@gmail.com>
2023-10-12 13:29:31 -0700

    no hhome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+import Breakdown from "./Breakdown/page";
+import { useEffect } from "react";
+
 export default function Home() {
-  return (
-    <>
-      <div className='flex justify-center p-2 w-full bg-slate-900 text-emerald-400 text-bold text-2xl rounded-lg shadow-md shadow-slate-900'>Main Page</div>
-    </>
-  )
+  // return (
+  //   <>
+  //     <div className='flex justify-center p-2 w-full bg-slate-900 text-emerald-400 text-bold text-2xl rounded-lg shadow-md shadow-slate-900'>Main Page</div>
+  //   </>
+  // )
+  const { push } = useRouter();
+  useEffect(() => {
+    push("/Breakdown");
+  }, []);
+  return (<></>)
 }
